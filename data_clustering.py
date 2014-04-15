a9878aaafd6934bbef1ec53ae802ef1988468a2c
Nedko Stefanov Nedkov <nedko.stefanov.nedkov@gmail.com>
2014-04-15 20:30:16 +0200

    Handling case where the transaction id is not unique among two close operations

--- data_clustering.py
+++ data_clustering.py
@@ -1,9 +1,9 @@
-#############################
-#   Filename: script.py     #
-#   Nedko Stefanov Nedkov   #
-#   nedko.nedkov@inria.fr   #
-#   April 2014              #
-#############################
+####################################
+#   Filename: data_clustering.py   #
+#   Nedko Stefanov Nedkov          #
+#   nedko.nedkov@inria.fr          #
+#   April 2014                     #
+####################################
 
 ''' docstring to be filled '''
 
@@ -11,7 +11,7 @@ import os
 from time import strptime
 from calendar import timegm
 
-trace_files_dir = '/home/nedko/Inria/DNStraces/'
+trace_files_dir = '/home/nedko/Inria/DNS_traces/'
 trace_files_name_prefix = 'dns2-sop-00'
 trace_files_path_prefix = '%s%s' % (trace_files_dir, trace_files_name_prefix)
 
@@ -72,12 +72,13 @@ if __name__ == '__main__':
         content_dir = './content_%s_%s_%s' % (domain, ip_version, class_type)
         if not os.path.isdir(content_dir):
             os.makedirs(content_dir)
-        
+
         # MAJOR HYPOTHESIS: I assume that for each transaction there is a
         #                   request and a corresponding response
         transaction_status = dict()
-        trace_types = list()
+        trace_types_num = dict()
         open_user_transactions = list()
+        invalid_operations = list()
         in_view_traces = list()
         in_view_pending_traces_queue = list()
         ex_view_traces = list()
@@ -85,7 +86,7 @@ if __name__ == '__main__':
 
         for i in range(9):
             trace_file = '%s%s' % (trace_files_path_prefix, str(i))
-            
+
             if i == 0:
                 print 'Processing content "%s":' % str(content)
             print '\tCurrently processing file %s' % trace_file
@@ -102,7 +103,7 @@ if __name__ == '__main__':
                     if trace_domain == content_domain and \
                        trace_class_type == content_class_type and \
                        trace_ip_version == content_ip_version:
-                           
+
                         #print '%s\n%s\n%s\n%s\n\n%s\n\n\n' % (in_view_traces, in_view_pending_traces_queue, ex_view_traces, ex_view_pending_traces_queue, open_user_transactions)
 
                         datetime = trace_args[1]
@@ -134,8 +135,11 @@ if __name__ == '__main__':
                         elif not is_request and not dns_is_dest:
                             trace_type = 4
                             assert ttl is not None
-                            
-                        trace_types.append(trace_type)
+
+                        try:
+                            trace_types_num[trace_type] += 1
+                        except:
+                            trace_types_num[trace_type] = 1
 
                         trace_rec = (trace_type, [datetime])
                         if ttl is not None:
@@ -146,17 +150,38 @@ if __name__ == '__main__':
 
                         if dns_is_dest:
                             if transaction_id in transaction_status:
-                                is_open, _ = transaction_status[transaction_id]
+                                is_open, _, caution_data = transaction_status[transaction_id]
+
+                                cd_is_user, cd_src, cd_secs = caution_data
+                                if is_open and cd_is_user == True and cd_src == src and \
+                                   acc_secs_since_epoch-cd_secs < 3:
+                                    trace_types_num[trace_type] -= 1
+                                    invalid_operations.append(datetime)
+                                    continue
+                                if not is_open and cd_is_user == True and cd_src == src and \
+                                   acc_secs_since_epoch-cd_secs < 3 and answers_count > 0:
+                                    trace_types_num[trace_type] -= 1
+                                    invalid_operations.append(datetime)
+                                    continue
+
                                 is_user = not is_open
                             else:
                                 is_user = True
-                                
+
                             if is_user:
                                 is_internal = bool(int(trace_args[11]))
 
                         else:
                             if transaction_id in transaction_status:
-                                is_open, _ = transaction_status[transaction_id]
+                                is_open, _, caution_data = transaction_status[transaction_id]
+
+                                cd_is_user, cd_src, cd_secs = caution_data
+                                if not is_open and cd_is_user == True and cd_src == dest and \
+                                   acc_secs_since_epoch-cd_secs < 3:
+                                    trace_types_num[trace_type] -= 1
+                                    invalid_operations.append(datetime)
+                                    continue
+
                                 is_user = not not is_open
                             else:
                                 is_user = False
@@ -164,12 +189,19 @@ if __name__ == '__main__':
                             if is_user:
                                 is_internal = bool(int(trace_args[12]))
 
+
+                        #assert not (src == 'H153818' and transaction_id == '0x90a8' and trace_domain == 'N214' and trace_args[0] == '4310608'), [transaction_status[transaction_id], is_open, is_internal, is_user, is_request]
+#                        if answers_count > 0 and ttl is not None:
+#                            assert False, trace_args
+#                            invalid_requests.append(datetime)
+#                            continue
+
                         if is_user and is_request:
-                            
+
                             # mapping
                             open_user_transactions.append([src, transaction_id, None])
                             is_open = True
-                            transaction_status[transaction_id] = [is_open, is_internal]
+                            transaction_status[transaction_id] = [is_open, is_internal, [is_user, src, acc_secs_since_epoch]]
 
                             # writing
                             if is_internal == True:
@@ -186,7 +218,7 @@ if __name__ == '__main__':
                                     ex_view_pending_traces_queue.append([trace_rec, transaction_id, pending])
                             else:
                                 assert False
-                            
+
                         elif is_user and not is_request:
 
                             # mapping
@@ -213,17 +245,18 @@ if __name__ == '__main__':
                                 assert False
 
                         elif not is_user and is_request:
-                            
+
                             # mapping
                             assert len(set([True for _, _, ass_trans_id in open_user_transactions if ass_trans_id is None])) == 1, "%s\n%s" % (line, open_user_transactions)
                             last_open_transaction = open_user_transactions[-1]
                             _, user_trans_id, ass_trans_id = last_open_transaction
                             assert ass_trans_id is None
                             open_user_transactions[-1][2] = transaction_id
-                            user_trans_is_open, user_trans_is_internal = transaction_status[user_trans_id]
+                            user_trans_is_open, user_trans_is_internal, _ = transaction_status[user_trans_id]
                             assert user_trans_is_open
-                            transaction_status[transaction_id] = [True, user_trans_is_internal]
+                            is_open = True
                             is_internal = user_trans_is_internal
+                            transaction_status[transaction_id] = [is_open, is_internal, [is_user, src, acc_secs_since_epoch]]   # I am not sure if there is point in keeping the 3rd data
 
                             # writing
                             if is_internal == True:
@@ -234,9 +267,9 @@ if __name__ == '__main__':
                                 ex_view_pending_traces_queue.append([trace_rec, transaction_id, pending])
                             else:
                                 assert False
-                            
+
                         elif not is_request and not is_user:
-                            
+
                             # mapping
                             if answers_count == 0:
                                 for j, open_trans in enumerate(open_user_transactions):
@@ -288,24 +321,24 @@ if __name__ == '__main__':
                                     flush_buffer(ex_view_pending_traces_queue, ex_view_traces)
                                 else:
                                     assert False
-                                    
+
                             #print '%s\n%s\n%s\n%s\n%s\n\n' % (is_internal, in_view_pending_traces_queue, ex_view_pending_traces_queue, open_user_transactions, [len(in_view_traces), len(ex_view_traces), trace_type])
-                                    
-        print 'Number of type "1" traces: %s' % len([True for trace_type in trace_types if trace_type == 1])
-        print 'Number of type "2" traces: %s' % len([True for trace_type in trace_types if trace_type == 2])
-        print 'Number of type "3" traces: %s' % len([True for trace_type in trace_types if trace_type == 3])
-        print 'Number of type "4" traces: %s\n\n' % len([True for trace_type in trace_types if trace_type == 4])
-        
+
+        print 'Number of type "1" traces: %s' % trace_types_num[1]
+        print 'Number of type "2" traces: %s' % trace_types_num[2]
+        print 'Number of type "3" traces: %s' % trace_types_num[3]
+        print 'Number of type "4" traces: %s\n\n' % trace_types_num[4]
+
         print 'Number of type "1" traces: %s' % len([True for trace_type, data in in_view_traces if trace_type == 1])
         print 'Number of type "2" traces: %s' % len([True for trace_type, data in in_view_traces if trace_type == 2])
         print 'Number of type "3" traces: %s' % len([True for trace_type, data in in_view_traces if trace_type == 3])
         print 'Number of type "4" traces: %s\n\n' % len([True for trace_type, data in in_view_traces if trace_type == 4])
-        
+
         print 'Number of type "1" traces: %s' % len([True for trace_type, data in ex_view_traces if trace_type == 1])
         print 'Number of type "2" traces: %s' % len([True for trace_type, data in ex_view_traces if trace_type == 2])
         print 'Number of type "3" traces: %s' % len([True for trace_type, data in ex_view_traces if trace_type == 3])
         print 'Number of type "4" traces: %s\n\n' % len([True for trace_type, data in ex_view_traces if trace_type == 4])
-        
+
 
         if in_view_traces:
             assert not in_view_pending_traces_queue
@@ -316,7 +349,7 @@ if __name__ == '__main__':
             in_view_req_miss_file_name = '%s/req_miss_%s_%s_%s.txt' % (in_view_dir, domain, ip_version, class_type)
             in_view_res_arr_file_name = '%s/res_arr_%s_%s_%s.txt' % (in_view_dir, domain, ip_version, class_type)
             in_view_res_miss_file_name = '%s/res_miss_%s_%s_%s.txt' % (in_view_dir, domain, ip_version, class_type)
-            
+
             for trace_type, data in in_view_traces:
                 if trace_type == 1:
                     with open(in_view_req_arr_file_name, 'a') as fp:
@@ -327,9 +360,11 @@ if __name__ == '__main__':
                 elif trace_type == 3:
                     with open(in_view_res_miss_file_name, 'a') as fp:
                         fp.write('%s\t%s\n' % (data[0], data[1]))
+                        #fp.write('%s\n' % data[0])
                 elif trace_type == 4:
                     with open(in_view_res_arr_file_name, 'a') as fp:
                         fp.write('%s\t%s\n' % (data[0], data[1]))
+                        #fp.write('%s\n' % data[0])
 
         if ex_view_traces:
             assert not ex_view_pending_traces_queue
@@ -354,3 +389,10 @@ if __name__ == '__main__':
                 elif trace_type == 4:
                     with open(ex_view_res_miss_file_name, 'a') as fp:
                         fp.write('%s\t%s\n' % (data[0], data[1]))
+
+        if invalid_operations:
+            invalid_operations_log_name = '%s/invalid_operations.log' % content_dir
+
+            with open(invalid_operations_log_name, 'w') as fp:
+                for datatime in invalid_operations:
+                    fp.write('%s\n' % datatime)
---
+++ pre-commit.sh
@@ -0,0 +1,134 @@
+#!/bin/bash
+
+###############################
+#   Filename: pre-commit.sh   #
+#   Nedko Stefanov Nedkov     #
+#   nedko.nedkov@inria.fr     #
+#   April 2014		      #
+###############################
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+declare -a FLAGS=(
+1 # (1) show options
+0 # (2) delete trailing whitespaces from modified files
+0 # (3) run pychecker on modified files
+0 # (4) run tests
+
+### WHEN ADDING NEW FUNCTIONALITY TOUCH HERE ###
+)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+
+declare -A colors
+colors["red"]="\e[0;31m"
+colors["blue"]="\e[00;36m"
+colors["dark_green"]="\e[00;32m"
+colors["yellow"]="\e[01;33m"
+
+function print() {
+	if (( $# == 2 )); then
+		echo -e "${colors[$1]}$2\e[00m"
+	else
+		echo -e $1
+	fi
+}
+
+
+if (( $# > 0 )); then
+	declare -a FLAGS=(0 0 0 0) ### WHEN ADDING NEW FUNCTIONALITY TOUCH HERE ###
+	for arg do
+   		case $arg in
+    		1)   ((FLAGS[0]=1));;
+    		2)   ((FLAGS[1]=1));;
+   		3)   ((FLAGS[2]=1));;
+		4)   ((FLAGS[3]=1));;
+
+	        ### WHEN ADDING NEW FUNCTIONALITY TOUCH HERE ###
+   		esac
+	done
+fi
+
+######################################################################################################
+# show options
+
+function show_options() {
+	print "blue" "\n*** Showing options ***"
+	print "(1) show options\n"
+	print "(2) delete trailing whitespaces from modified files\n"
+	print "(3) run pychecker on modified files\n"
+	print "(4) run tests\n"
+
+	### WHEN ADDING NEW FUNCTIONALITY TOUCH HERE ###
+}
+
+######################################################################################################
+# delete trailing whitespaces from modified files
+
+function delete_trail_whitespaces_modified_files() {
+	print "blue" "\n*** Deleting trailing whitespaces from modified files ***"
+	FILES=(`git status | awk '$1 == "modified:" { print head$2 }'`)
+	for file in ${FILES[@]}; do
+		print "dark_green" "Deleting trailing whitespaces from file: $file"
+		sed -i '' -e's/[ \t]*$//' $file >& /dev/null
+	done
+	echo
+}
+
+######################################################################################################
+# run pychecker on modified files
+
+function check_modified_files() {
+	print "blue" "\n*** Running pychecker on modified files ***"
+	FILES=(`git status | awk '$1 == "modified:" { print head$2 }'`)
+	for file in ${FILES[@]}; do
+		print "dark_green" "\nPychecking modified file: $file\n"
+		pychecker $file
+	done
+	echo
+}
+
+######################################################################################################
+# run tests
+
+function run_tests() {
+	CLUSTER_FILES=("req_arr_cluster":"req_arr_214_v4_0x0001"
+		       "req_miss_cluster":"req_miss_214_v4_0x0001"
+		       "res_miss_cluster":"res_miss_214_v4_0x0001"
+		       "res_arr_cluster":"res_arr_214_v4_0x0001")
+	print "blue" "\n*** Running tests ***"
+	for file in ${CLUSTER_FILES[@]}; do
+		key="${file%%:*}"
+		value="${file##*:}"
+		print "dark_green" "\n$key:"
+		diff "content_214_v4_0x0001/internal_view/$value.txt" "/home/nedko/Inria/test/$value.txt" >/dev/null
+		if (( $? == 1 )); then
+			print "red" "Not correct"
+			diff "content_214_v4_0x0001/internal_view/$value.txt" "/home/nedko/Inria/test/$value.txt" > "logs/$value.diff"
+		else
+			echo "Correct"
+		fi
+	done
+	echo
+}
+
+######################################################################################################
+
+if (( FLAGS[0] == 1 )); then
+	show_options
+fi
+
+if (( FLAGS[1] == 1 )); then
+	delete_trail_whitespaces_modified_files
+fi
+
+if (( FLAGS[2] == 1 )); then
+	check_modified_files
+fi
+
+if (( FLAGS[3] == 1 )); then
+	run_tests
+fi
+
+### WHEN ADDING NEW FUNCTIONALITY TOUCH HERE ###
+
