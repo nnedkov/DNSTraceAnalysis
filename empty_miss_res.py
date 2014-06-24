ba01c05c76ae7da8a18463df2aa8a73e423f2d2e
Nedko Stefanov Nedkov <nedko.stefanov.nedkov@gmail.com>
2014-06-24 15:09:55 +0200

    Temporary script edited

--- empty_miss_res.py
+++ empty_miss_res.py
@@ -11,27 +11,31 @@ from trace import Trace
 
 
 
-def reducer(reducer_input, qcontent, separator='\t'):
+def reducer(reducer_input, contents, separator='\t'):
 
     def process_content(content, traces):
         traces = sorted(traces, key=lambda trace: trace.get_secs_value())
-        has_valid_res_miss = bool([True for trace in traces if trace.answers_num != 0])
+
+        issues_req_miss = bool([True for trace in traces if trace.type in [3, 4]])
         max_ttl = max([trace.ttl for trace in traces if trace.ttl != None])
 
-        return has_valid_res_miss, max_ttl
+        return issues_req_miss, max_ttl
 
 
     last_content = None
     traces = list()
+    results = dict()
+    i = 0
 
     for input_line in reducer_input:
         content, trace_str = input_line.split(separator, 1)
         trace_str = separator.join(trace_str.split(SEPARATOR))
 
-        if content != qcontent:
+        if content not in contents:
             continue
 
         trace = Trace(trace_str)
+        trace.fill_out()
 
         if last_content is None:
             last_content = content
@@ -40,13 +44,19 @@ def reducer(reducer_input, qcontent, separator='\t'):
             traces.append(trace)
             continue
 
-        return process_content(last_content, traces)
+        result = process_content(last_content, traces)
+        results[content] = result
+        print '%s/%s' % (i, len(contents))
+        i += 1
 
         last_content = content
         traces = [trace]
 
     if traces:
-        return process_content(last_content, traces)
+        result = process_content(last_content, traces)
+        results[content] = result
+
+    return results
 
 
 def main(separator='\t'):
@@ -54,28 +64,29 @@ def main(separator='\t'):
 
     reducer_input_filename = './reducer_input.txt'
     reducer_input = list()
-    
+
     with open(reducer_input_filename) as fp:
         for line in fp:
             reducer_input.append(line.strip())
 
     print 'Processing the popularity file...'
-    
+
     popularity_filename = './popularity.txt'
     new_popularity_filename = './new_popularity.txt'
+    content_arguments = dict()
 
     with open(popularity_filename) as fpi:
-        for i, line in enumerate(fpi):
+        for line in fpi:
             args = line.strip().split(separator)
             assert len(args) == 8
             req_miss_num = int(args[2])
-            
+
             if req_miss_num != 0:
                 with open(new_popularity_filename, 'a') as fpo:
                     fpo.write(line)
 
-            content_args = args[0].split('_')[:3]
-            
+            content_args = args[0].split('_')
+
             assert content_args[1] in ['v4', 'v6', 'sec']
             if content_args[1] == 'v4':
                 content_args[1] = '0x0001'
@@ -83,14 +94,28 @@ def main(separator='\t'):
                 content_args[1] = '0x001c'
             elif content_args[1] == 'sec':
                 content_args[1] = '0x000c'
-            content = '_'.join(content_args)
+            content = '_'.join([content_args[0], content_args[2], content_args[1]])
 
-            has_valid_res_miss, max_ttl = reducer(reducer_input, content)
-            args[2] = str(has_valid_res_miss)
-            args[7] = "[ >= '%s']" % max_ttl
-            
-            with open(new_popularity_filename, 'a') as fpo:
-                fpo.write(separator.join(args) + '\n')
+            content_arguments[(content, content_args[3])] = args
+
+    contents = set([content for content, _ in content_arguments.keys()])
+    results = reducer(reducer_input, contents)
+    keyerrors = list()
+
+    for content, view in content_arguments.keys():
+        try:
+            issues_req_miss, max_ttl = results[content]
+        except KeyError:
+            keyerrors.append(content)
+            continue
+        args = content_arguments[(content, view)]
+        args[2] = str(issues_req_miss)
+        args[7] = "[ >= '%s']" % max_ttl
+
+        with open(new_popularity_filename, 'a') as fpo:
+            fpo.write(separator.join(args) + '\n')
+
+    print len(keyerrors), keyerrors
 
 
 if __name__ == '__main__':
