9e3920ca25c025b8d7724b04ec700cde0dfeb887
Nedko Stefanov Nedkov <nedko.stefanov.nedkov@gmail.com>
2014-07-21 14:54:54 +0200

    Refactored/cleaned tester.py

--- tester.py
+++ tester.py
@@ -7,13 +7,27 @@
 
 from config import SEPARATOR_1
 
-from os import sys, walk
+from os import sys, walk, path
 
+
+
+def load_res_cluster(filepath):
+    cluster = list()
+
+    with open(filepath) as fp:
+
+        for line in fp:
+            args = line.strip().split(SEPARATOR_1)
+            secs = args[0]
+            ttl = args[1]
+            cluster.append((secs, ttl))
+
+    return cluster
 
 
 def res_arr_cluster_is_valid(res_arr_cluster):
     valid_until = None
-    prev_secs = None
+    prev_time = None
     prev_ttl = None
 
     for current_time_str, current_ttl_str in res_arr_cluster:
@@ -21,13 +35,14 @@ def res_arr_cluster_is_valid(res_arr_cluster):
         current_ttl = float(current_ttl_str)
 
         if valid_until is not None:
-            secs_diff = current_time - prev_secs
+            time_diff = current_time - prev_time
             ttl_diff = prev_ttl - current_ttl + 2
-            if not current_time > valid_until and secs_diff > ttl_diff:
-                return False, [current_time_str, secs_diff, ttl_diff]
+
+            if not current_time > valid_until and time_diff > ttl_diff:
+                return False, [current_time_str, time_diff, ttl_diff]
 
         valid_until = current_time + current_ttl - 1
-        prev_secs = current_time
+        prev_time = current_time
         prev_ttl = current_ttl
 
     return True, None
@@ -51,43 +66,31 @@ def res_miss_cluster_is_valid(res_miss_cluster):
 
 def main():
 
-    def load_res_cluster(filename):
-        cluster = list()
-
-        with open(filename) as fp:
-
-            for line in fp:
-                args = line.strip().split(SEPARATOR_1)
-                secs = args[0]
-                ttl = args[1]
-                cluster.append((secs, ttl))
-
-        return cluster
-
-
     if len(sys.argv) < 2:
         raise Exception('No content directory specified!')
 
-    content_dir = sys.argv[1]
+    content_dirs = sys.argv[1:]
 
-    for dirpath, _, filenames in walk(content_dir):
+    for content_dir in content_dirs:
 
-        for filename in filenames:
-            if 'for_tests' in dirpath or not 'res_' in filename:
-                continue
+        for root, _, filenames in walk(content_dir):
 
-            is_res_arr = True if 'res_arr' in filename else False
-            cluster = load_res_cluster(dirpath + '/' + filename)
+            for filename in filenames:
+                if not 'res_' in filename or 'for_tests' in root:
+                    continue
 
-            if is_res_arr:
-                success, invalid_rec = res_arr_cluster_is_valid(cluster)
-            else:
-                success, invalid_rec = res_miss_cluster_is_valid(cluster)
+                is_res_arr = True if 'res_arr' in filename else False
+                cluster = load_res_cluster(path.join(root, filename))
 
-            if success:
-                print 'Success!'
-            else:
-                print '%s : FAILURE!\t\t%s' % (filename, str(invalid_rec))
+                if is_res_arr:
+                    success, invalid_rec = res_arr_cluster_is_valid(cluster)
+                else:
+                    success, invalid_rec = res_miss_cluster_is_valid(cluster)
+
+                if success:
+                    print 'Success!'
+                else:
+                    print '%s: FAILURE!\t(%s)' % (filename, str(invalid_rec))
 
 
 
